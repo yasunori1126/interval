cb22066386b30d3adf7a8ee2e37f2e478a5a021f
Yasunori Yura <yasunoriyura@Yasunori-no-MacBook-Pro.local>
2017-08-08 23:21:15 +0900

    ADD

---
+++ interval.js
@@ -0,0 +1,22 @@
+document.addEventListener('DOMContentLoaded',function(){
+
+	var timer = window.setInterval(
+
+		function(){
+
+
+			var dat = new Date();
+
+			document.getElementById('result').textContent = dat.toLocaleTimeString();
+
+		}
+
+	,1000)
+
+	document.getElementById('btn').addEventListener('click',function(){
+
+		window.clearInterval(timer);
+
+	},false);
+
+},false);
